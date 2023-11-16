77e445ceb79fb9a04b8b9338bb3af46706f1eda3
mcr3 <m.marcelo_98@hotmail.com>
2023-11-15 22:15:42 -0300

    [TEST - mrc3]

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -2,26 +2,22 @@ package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 public class GettingStartedTest {
-
     @Test
     public void addFiveTo20() {
         int result = new GettingStarted().addFive(20);
         Assertions.assertEquals(25,result);
     }
-
-//    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
-
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    //UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
+    @Test
+    // Add the correct numbers
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+    @Test
+    // Write this test
+    public void addFiveToMinusTwenty() {
+        int result = new GettingStarted().addFive(-20);
+    }
 }
